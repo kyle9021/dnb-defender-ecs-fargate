b87b62de3c95db90925152c55d1fb447902cc24f
Kyle Butler <54778108+kyle9021@users.noreply.github.com>
2021-12-10 13:00:16 -0500

    Revert "Prisma Cloud fix config: aws_s3_bucket.fargate  and 2 more"

--- logs.tf
+++ logs.tf
@@ -7,14 +7,4 @@ resource "aws_s3_bucket" "fargate" {
   bucket        = "dnb-prisma-fargate"
   acl           = "private"
   force_destroy = "true"
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "aws:kms"
-      }
-    }
-  }
-  versioning {
-    enabled = true
-  }
 }
